6c4754fb252de7d6489912ca40e3339e789b1070
irpo0001@stud.kea.dk <irpo0001@stud.kea.dk>
2024-10-10 01:44:28 +0200

    del 4 - opstart

--- src/RangedWeapon.java
+++ src/RangedWeapon.java
@@ -3,7 +3,7 @@ public class RangedWeapon extends Weapon {
     private int uses;
     private int damage;
 
-    public RangedWeapon(String longName, String shortName, int uses) {
+    public RangedWeapon(String longName, String shortName, int uses, int damage) {
         super(longName, shortName);
         this.uses = uses;
         this.damage = damage;
@@ -13,17 +13,34 @@ public class RangedWeapon extends Weapon {
         return uses;
     }
     @Override
+    public int getDamage() {
+    return damage;
+    }
+    public void use () {
+        if (canUse()){
+            uses --;
+            System.out.println();
+        }
+    }
+}
+
+    @Override
     public boolean canUse(){
         return uses > 0;
-    }
-    public void use() {
-        if (uses > 0) uses--;
-        else System.out.println("This weapon can no longer be used");
+
+    } if(uses > 0) uses--;
+     System.out.println("This weapon can no longer be used"); {
+
     }
     public void use(){
+
         if(canUse()) {
             uses--;
-            System.out.println("fire your " + getLongName() + "!");
+            System.out.println("fire your " + getLongName() + "!");{
+
+            } else if  (uses > 0) uses--; {
+            else System.out.println("This weapon can no longer be used");
+
         } else {
             System.out.println("you have now more ammunition");
         }
