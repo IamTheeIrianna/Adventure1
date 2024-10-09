0a14b1aff446cca868443a1ee313663f8e27d491
irpo0001@stud.kea.dk <irpo0001@stud.kea.dk>
2024-10-09 11:24:18 +0200

    Room

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Adventure adventure = new Adventure();
-        adventure.startGame();
+        Adventure adventureGame = new Adventure();
+        adventureGame.startGame(); // Start the game after displaying the welcome message
     }
 }
